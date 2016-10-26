843a64793b693cf37ed95ea18d59367001b3c98e
yfl <554665488@qq.com>
2016-10-26 15:29:25 +0800

    一步上传图片

--- upload.php
+++ upload.php
@@ -6,10 +6,10 @@
  * Time: 9:01
  */
 
-namespace demo;
 
 
-class upload
-{
-
-}
+print_r($_FILES['fileInput']);
+$img_name=$_FILES['fileInput']['name'];
+$img_name_arr=explode('.',$img_name);
+var_dump($_POST['id']);
+var_dump(move_uploaded_file($_FILES['fileInput']['tmp_name'],"./img/".time().rand(1,100).'.'.end($img_name_arr)));
